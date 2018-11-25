775cc0bce89516e381394b6de70b4da3ea84f59d
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-25 10:21:39 +0000

    Requirements 3-4 tested

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -7,23 +7,41 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq 9
     end
-  end
 
-  it 'Decreases in quality by 1 before the sell-by date' do
-    items = [Item.new('+5 Dexterity Vest', 10, 50)]
-    GildedRose.new(items).update_quality
-    expect(items[0].quality).to eq 49
-  end
+    it 'Decreases in quality by 1 before the sell-by date' do
+      items = [Item.new('+5 Dexterity Vest', 10, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 49
+    end
 
-  it "Once the sell by date has passed, Quality degrades twice as fast" do
-    items = [Item.new('+5 Dexterity Vest', 0, 50)]
-    GildedRose.new(items).update_quality
-    expect(items[0].quality).to eq 48
-  end
+    it 'Once the sell by date has passed, Quality degrades twice as fast' do
+      items = [Item.new('+5 Dexterity Vest', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 48
+    end
 
-  it "The Quality of an item is never negative" do
-    items = [Item.new('+5 Dexterity Vest', 0, 0)]
-    GildedRose.new(items).update_quality
-    expect(items[0].quality).to eq 0
+    it 'The Quality of an item is never negative' do
+      items = [Item.new('+5 Dexterity Vest', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
+
+    it 'Aged Brie increases in Quality the older it gets' do
+      items = [Item.new('Aged Brie', 5, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 11
+    end
+
+    it 'Aged Brie increases in Quality past sell-by date' do
+      items = [Item.new('Aged Brie', 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 12
+    end
+
+    it "The Quality of an item is never more than 50" do
+      items = [Item.new('Aged Brie', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 50
+    end
   end
 end
