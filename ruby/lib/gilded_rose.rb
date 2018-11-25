b409a5a1a644f66ba2f720efdc12b6329200e9f7
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-25 11:59:02 +0000

    Refactored half the tests

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -6,47 +6,60 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-            if item.sell_in < 6
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-          end
-        end
+      item.sell_in -= 1
+      if item.name == '+5 Dexterity Vest' || item.name == 'Elixir of the Mongoose'
+        item.quality -= item.sell_in > 0 ? 1 : 2
+        item.quality = 0 if item.quality < 0
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          item.quality = item.quality + 1 if item.quality < 50
-        end
-      end
-      if item.name.include? 'Conjured'
-        item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
-        # item.quality -= 1 if item.sell_in > 0
+      if item.name == "Aged Brie"
+        item.quality += 1 if item.sell_in < 0
+        item.quality += 1
       end
     end
   end
+
+  # def update_quality
+  #   @items.each do |item|
+  #     if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+  #       if item.quality > 0
+  #         if item.name != 'Sulfuras, Hand of Ragnaros'
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.sell_in < 11
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #           if item.sell_in < 6
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != 'Sulfuras, Hand of Ragnaros'
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != 'Aged Brie'
+  #         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.quality > 0
+  #             if item.name != 'Sulfuras, Hand of Ragnaros'
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         item.quality = item.quality + 1 if item.quality < 50
+  #       end
+  #     end
+  #     if item.name.include? 'Conjured'
+  #       item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
+  #     end
+  #   end
+  # end
 end
--- ruby/lib/inventory.rb
+++ ruby/lib/inventory.rb
@@ -16,7 +16,7 @@ items = [
   Item.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 6), # <-- :O
 ]
 
-days = 2
+days = 4
 days = ARGV[0].to_i + 1 unless ARGV.empty?
 
 gilded_rose = GildedRose.new items
