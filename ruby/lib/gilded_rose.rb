670d7f9b0282d4cf89e2da6ba393629a0fb717db
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-25 12:11:18 +0000

    Gilded Rose fully refactored

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -6,14 +6,26 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      item.sell_in -= 1
+      item.sell_in -= 1 unless item.name.include? 'Sulfuras'
       if item.name == '+5 Dexterity Vest' || item.name == 'Elixir of the Mongoose'
         item.quality -= item.sell_in > 0 ? 1 : 2
         item.quality = 0 if item.quality < 0
       end
-      if item.name == "Aged Brie"
+      if item.name == 'Aged Brie'
         item.quality += 1 if item.sell_in < 0
         item.quality += 1
+        item.quality = 50 if item.quality > 50
+      end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality += 1
+        item.quality += 1 if item.sell_in < 10
+        item.quality += 1 if item.sell_in < 5
+        item.quality = 0 if item.sell_in < 0
+      end
+
+      if item.name.include? 'Conjured'
+        item.quality -= 2
+        item.quality -= 2 if item.sell_in < 0
       end
     end
   end
